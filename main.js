7c1f4e86537fd5ae4695126306ff105a2e056cdc
realtimexx93 <81872364+realtimexx93@users.noreply.github.com>
2022-01-23 12:55:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,5 +15,24 @@ function take_snapshot()
         document.getElementById("result").innerHTML = '<img id="captured_image" src=" '+data_uri+'"/>';
     });
 }
-console.log('m15 version:', m15.version);
-classifier = m15.imageClassifier('httpsteachablemachine.withgoogle.commodelsklqjo_4PQ/model.json',modelLoaded);
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hh-vu_DLo/model.json',modelLoaded);
+function modelLoaded() {
+    console.log ('Model Loaded');
+
+}
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+    }
+    }
